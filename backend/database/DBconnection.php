ed88890ff74474c93237dea7bfbb9c41b77a147e
baker-james <bakerjames1995@gmail.com>
2018-03-01 10:05:05 +0000

    Remove echo in DBconnection.php, spoils json format

--- backend/database/DBconnection.php
+++ backend/database/DBconnection.php
@@ -17,7 +17,7 @@ try {
     $connection = new PDO('mysql:localhost;port=3306;dbname=flyguys', 'flyguysUser', 'Kingston2017!');
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo 'Connection success';
+    //echo 'Connection success';
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
