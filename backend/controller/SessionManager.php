3fc72fabb82593396b782c378e305907c2fd48c4
James Baker <yakoobi@hotmail.co.uk>
2018-03-20 19:56:08 +0000

    Echo cookie details in SessionManager.php

--- backend/controller/SessionManager.php
+++ backend/controller/SessionManager.php
@@ -3,6 +3,13 @@ require_once("../model/Account.php");
 require_once("../model/Cookie.php");
 header("Access-Control-Allow-Origin: *");
 
+
 $cookie = new Cookie();
+
+// Insert new account into DB
+
+echo json_encode($cookie);
+
+
 
 
