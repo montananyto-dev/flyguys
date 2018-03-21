70d08c44c9960a237570d591744ad185fa23142e
James Baker <yakoobi@hotmail.co.uk>
2018-03-21 15:30:15 +0000

    Revert changes

--- src/controller/locations/names.php
+++ src/controller/locations/names.php
@@ -3,7 +3,7 @@ require_once("../../model/Location.php");
 require_once("../../database/DAO.php");
 header("Access-Control-Allow-Origin: *");
 
-$allLocations = DAO::getInstance()->getLocations('id', 4);
+$allLocations = DAO::getInstance()->getLocations();
 
 $names = array();
 foreach($allLocations as $location) {
