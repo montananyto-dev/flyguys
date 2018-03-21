992bd08424baad125b27e3825810a8b700b5e49a
montananyto-dev <montananyto@hotmail.com>
2018-03-21 15:35:01 +0000

    VIEW FLIGHTS WORKING

--- backend/controller/temp.php
+++ backend/controller/temp.php
@@ -3,9 +3,5 @@ require_once("../model/Location.php");
 require_once("../database/DAO.php");
 header("Access-Control-Allow-Origin: *");
 
-$region = DAO::getInstance()->getRegions('id'. 1, true);
-
-
-
-echo json_encode($all);
+DAO::getInstance()->getAllFlightsByRegion('Domestic');
 
--- backend/database/DAO.php
+++ backend/database/DAO.php
@@ -5,13 +5,15 @@ require_once(__DIR__ . '/../model/Connection.php');
 require_once(__DIR__ . '/../model/Region.php');
 require_once(__DIR__ . '/../model/Account.php');
 
-class DAO {
+class DAO
+{
 
     private static $instance;
 
     private $conn;
 
-    private function __construct() {
+    private function __construct()
+    {
         $serverName = "localhost";
         $username = "flyguys";
         $password = "Kingston2017!";
@@ -21,20 +23,21 @@ class DAO {
             // set the PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        }
-        catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
     }
 
-    public static function getInstance() {
-        if(!isset(static::$instance)) {
+    public static function getInstance()
+    {
+        if (!isset(static::$instance)) {
             static::$instance = new DAO;
         }
         return static::$instance;
     }
 
-    private function plainQuery($sql) {
+    private function plainQuery($sql)
+    {
         $statement = $this->conn->prepare($sql);
         $statement->execute();
         $results = $statement->fetchAll();
@@ -42,7 +45,8 @@ class DAO {
         return $results;
     }
 
-    private function classQuery($sql, $class) {
+    private function classQuery($sql, $class)
+    {
         $statement = $this->conn->prepare($sql);
         $statement->execute();
         $results = $statement->fetchAll(PDO::FETCH_CLASS, $class);
@@ -50,45 +54,49 @@ class DAO {
         return $results;
     }
 
-    public function getAccounts($property = 1, $value = 1, $singleReturn = false) {
+    public function getAccounts($property = 1, $value = 1, $singleReturn = false)
+    {
         $result = $this->classQuery("SELECT id, email, password, cookie, salt FROM account WHERE $property = '$value'", "Account");
 
-        if($singleReturn) {
+        if ($singleReturn) {
             return $result[0];
         }
 
         return $result;
     }
 
-    public function getRegions($property = 1, $value = 1, $singleReturn = false) {
+    public function getRegions($property = 1, $value = 1, $singleReturn = false)
+    {
         $result = $this->classQuery("SELECT id, name FROM region WHERE $property = '$value'", "Region");
 
-        if($singleReturn) {
+        if ($singleReturn) {
             return $result[0];
         }
 
         return $result;
     }
 
-    public function getLocations($property = 1, $value = 1, $singleReturn = false) {
+    public function getLocations($property = 1, $value = 1, $singleReturn = false)
+    {
         $result = $this->classQuery("SELECT * FROM location where $property = '$value'", "Location");
 
-        foreach($result as $row) {
+        foreach ($result as $row) {
             $region = $this->getRegions('id', $row->__get('region_id'), true);
             $row->region = $region;
         }
 
-        if($singleReturn) {
+        if ($singleReturn) {
             return $result[0];
         }
 
         return $result;
     }
 
-    public function getConnections($property = 1, $value = 1, $singleReturn = false) {
+    public function getConnections($property = 1, $value = 1, $singleReturn = false)
+    {
         $result = $this->classQuery("SELECT * FROM connection WHERE $property = '$value'", "Connection");
 
-        foreach($result as $row) {
+        foreach ($result as $row) {
             $fromLocation = $this->getLocations('id', $row->location_id1, true);
             $toLocation = $this->getLocations('id', $row->location_id2, true);
 
@@ -96,42 +104,45 @@ class DAO {
             $row->toLocation = $toLocation;
         }
 
-        if($singleReturn) {
+        if ($singleReturn) {
             return $result[0];
         }
 
         return $result;
     }
 
-    public function getFlights($property = 1, $value = 1, $singleReturn = false) {
+    public function getFlights($property = 1, $value = 1, $singleReturn = false)
+    {
 
         $result = $this->classQuery("SELECT * FROM flight WHERE $property = '$value'", "Flight");
 
-        foreach($result as $row) {
-            $connection = $this->getConnections('id',$row->connection_id, true);
-            $row->connection= $connection;
+        foreach ($result as $row) {
+            $connection = $this->getConnections('id', $row->connection_id, true);
+            $row->connection = $connection;
         }
 
-        if($singleReturn) {
+        if ($singleReturn) {
             return $result[0];
         }
 
         return $result;
     }
 
-    public function getConnectedLocations($location) {
+    public function getConnectedLocations($location)
+    {
         $connections = $this->getConnections('location_id1', $location->id);
 
         $toLocations = array();
-        foreach($connections as $connection) {
+        foreach ($connections as $connection) {
             array_push($toLocations, $connection->toLocation);
         }
 
         return $toLocations;
     }
 
-    public function getUpcomingFlights($fromLocation, $toLocation = null) {
-        if($toLocation == null) {
+    public function getUpcomingFlights($fromLocation, $toLocation = null)
+    {
+        if ($toLocation == null) {
             $toLocation = $this->getLocations();
         }
 
@@ -139,14 +150,42 @@ class DAO {
         $allFlights = $this->getFlights();
 
         $filteredFlights = array();
-        foreach($allFlights as $flight) {
-            if($flight->connection->fromLocation == $fromLocation
+        foreach ($allFlights as $flight) {
+            if ($flight->connection->fromLocation == $fromLocation
                 && $flight->connection->toLocation == $toLocation) {
                 array_push($filteredFlights, $flight);
             }
         }
 
         return $filteredFlights;
+    }
+
+    public function getAllFlightsByRegion($region)
+    {
+        if ($region == "Europe") {
+            $sql = "SELECT flight.*,r.name FROM flight INNER JOIN connection c ON flight.connection_id = c.id
+                                           INNER JOIN location l ON c.location_id1 = l.id
+                                           INNER JOIN location l1 ON c.location_id2 = l1.id
+                                           INNER JOIN region r ON l.region_id = r.id OR l1.region_id = r.id
+                                           WHERE r.name = 'Europe'";
+
+        } else {
+            $sql = "SELECT flight.*,r.name FROM flight INNER JOIN connection c ON flight.connection_id = c.id
+                                           INNER JOIN location l ON c.location_id1 = l.id
+                                           INNER JOIN location l1 ON c.location_id2 = l1.id
+                                           INNER JOIN region r ON l.region_id = r.id AND l1.region_id = r.id
+                                           WHERE r.name = 'Domestic' ";
+
+        }
+
+        $flights = $this->classQuery($sql, "Flight");
+
+        foreach ($flights as $row) {
+
+            $connection = $this->getConnections('id', $row->connection_id, true);
+            $row->connection = $connection;
+        }
+        return $flights;
     }
 
 }
